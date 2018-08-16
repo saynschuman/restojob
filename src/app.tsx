4d2941a4def59bfe4ca3d36404de6169c156d0f8
saynschuman <saynschuman@gmail.com>
2018-08-16 14:00:12 +0300

    comment

--- src/app.tsx
+++ src/app.tsx
@@ -12,6 +12,7 @@ import { IndexPageWrapper } from './pages/IndexPage/IndexPageWrapper';
 import { PublishVacancyPageWrapper } from './pages/PublishVacancyPage/PublishVacancyPageWrapper';
 import { CompanyPageWrapper } from './pages/CompanyPage/CompanyPageWrapper';
 moment.locale('ru');
+//some changes
 
 require('./styles/initial.styl');
 ReactDOM.render(
